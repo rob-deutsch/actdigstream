219efbb9dbcd27d481ca66b8f36a9d7936aa088f
rob <rob@rob.rob>
2015-08-04 06:37:25 -0400

    Logs to S3

--- main.js
+++ main.js
@@ -12,16 +12,27 @@ http.get(baseURL, function (res) {
   });
   res.on('end', function() {
     AWS.config.loadFromPath('./credentials.json');
-    var s3 = new AWS.S3({params: {Bucket: 'awsdigstream'} });
+    var s3 = new AWS.S3({params: {Bucket: 'actdigstream'} });
     $ = cheerio.load(body);
     var test = $('.post-list-item').find('h2').find('a').map(function(i, elem) {
       // Need double list so that it isn't fully unpacked
       var title    = $(this).text(),
           href     = $(this).attr('href'),
           fullLink = url.resolve(baseURL, $(this).attr('href')),
-          path     =  url.parse(url.resolve(baseURL, $(this).attr('href')))['path'];
-      console.log([title, href, fullLink, path]);
-      };
+          path     =  url.parse(fullLink)['path'].substring(1);
+      s3.headObject({Key: path}, function(err, data) {
+        if (err) {
+          var params = {
+            Key: path,
+            Body: 'a'
+          };
+          s3.putObject(params, function(err, data) {
+            if (err) console.log(err)
+            else     console.log("Successful marked on S3: " + path);
+          });
+        }
+        else console.log("Already exists: " + path);
+      });
     });
   });
 }).on('error', function(e) {
